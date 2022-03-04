8c4c3eecc9e7328f82ccb4d4c7a499679440c55a
Helen <le.helen19@gmail.com>
2022-03-04 15:01:57 -0800

    Fetched user data from API for robots when component is mounted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 // STATE: An object that describes your application
@@ -12,9 +11,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -24,7 +29,7 @@ class App extends Component {
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        });
+        })
         return (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
